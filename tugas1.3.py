85d2fbc68de80cdbd4773dd2b02c3a517d4d6320
fauzan-codes <azharfauzan472@gmail.com>
2026-02-12 20:27:42 +0700

    tugas1.3 Update

---
+++ tugas1.3.py
@@ -0,0 +1,12 @@
+namaD = input("Masukkan nama depan anda : ")
+namaB = input("Masukkan nama belakang anda : ")
+
+namaLengkap = f"{namaD} {namaB}"
+listNama = list(namaLengkap)
+listNama.reverse()
+
+namaTerbalik = ''.join(listNama)
+
+print("")
+print(f"Nama lengkap  : {namaLengkap}")
+print(f"Nama Terbalik : {namaTerbalik}")
