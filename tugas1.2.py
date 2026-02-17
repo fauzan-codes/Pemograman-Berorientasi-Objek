1ba66bed64064ba0a7132a93fe8dd1fce0bd9c3e
Fauzan <azharfauzan472@gmail.com>
2026-02-17 13:50:54 +0700

    update tugas 2

--- tugas1.2.py
+++ tugas1.2.py
@@ -1,19 +1,9 @@
 number = [0] * 10
-total = 0
-terbesar = 0
-terkecil = 9999999
 ganjil = 0
 genap = 0
 
-
 for i in range(10):
     number[i] = int(input(f"Masukkan angka ke-{i+1} : "))
-
-    total = total + number[i]
-    if terbesar < number[i]:
-        terbesar = number[i]
-    if terkecil > number[i]:
-        terkecil = number[i]
 
     if number[i] % 2 == 0:
         genap = genap + 1
@@ -21,9 +11,9 @@ for i in range(10):
         ganjil =+ ganjil + 1
 
 print()
-print(f"Total : {total}")
-print(f"Rata Rata : {total/len(number)}")
-print(f"angka Terbesar : {terbesar}")
-print(f"angka Terkecil : {terkecil}")
+print(f"Total : {sum(number)}")
+print(f"Rata Rata : {sum(number)/len(number)}")
+print(f"angka Terbesar : {max(number)}")
+print(f"angka Terkecil : {min(number)}")
 print(f"Jumlah Angka Ganjil : {ganjil}")
 print(f"Jumlah Angka Genap : {genap}")
