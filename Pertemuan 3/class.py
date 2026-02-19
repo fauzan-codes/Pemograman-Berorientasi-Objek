1b4d02640221213039b61a1e823ea950506468ca
fauzan-codes <azharfauzan472@gmail.com>
2026-02-19 13:19:24 +0700

    class update

--- Pertemuan 3/class.py
+++ Pertemuan 3/class.py
@@ -6,6 +6,17 @@ class mahasiswa:
         self.umur = umur   
 
     def introduction (self):
-        print(f"my name is {self.nama}, and i am from {self.jurusan}")
+        print(f"my name is {self.nama}, and i am from {self.jurusan}, and i am {self.umur} years old")
 
-    
+    def umur_2031(self):
+        print(f"Umur {self.nama} pada tahun 2031 adalah {self.umur + 5}")
+
+student1 = mahasiswa("deva", 1234, "mechanical laba laba sunda", 20)
+student2 = mahasiswa("ujang", 3872, "mechanical", 19)
+
+
+student1.introduction()
+student1.umur_2031()
+
+student2.introduction()
+student2.umur_2031()
