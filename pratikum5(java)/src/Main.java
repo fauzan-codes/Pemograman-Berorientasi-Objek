b69abe492e96bd83c7d9e1c09ffddd62097bcead
Fauzan <azharfauzan472@gmail.com>
2026-04-21 21:16:18 +0700

    add pratikum 5 java

--- pratikum5(java)/src/Main.java
+++ pratikum5(java)/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); 
 
         System.out.print("Nama: ");
         String nama = input.nextLine();
